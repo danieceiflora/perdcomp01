cbbc8a7f1b358fe64708f10bf1e92938c26b8053
danieceiflora <daniel_sousa_marques@hotmail.com>
2025-08-08 00:55:53 -0300

    Update virtual environment paths and binaries
    - Changed VIRTUAL_ENV path in activate and activate.bat scripts to point to the new location in OneDrive.
    - Updated pyvenv.cfg to reflect the new Python executable path and version.
    - Rebuilt binary files for django-admin, pip, python, and other related executables to ensure compatibility with the new environment setup.

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\danie\Desktop\perdcomp01\perdcomp01\venv
+set VIRTUAL_ENV=C:\Users\danie\OneDrive\Área de Trabalho\perdcomp01\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
